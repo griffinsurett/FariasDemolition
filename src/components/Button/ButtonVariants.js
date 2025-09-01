513281b45334f55362c5ab95db45926dc8cdb98c
griffinjoshs <leveloutusa@gmail.com>
2025-09-01 02:04:57 -0400

    initial commit

--- src/components/Button/ButtonVariants.js
+++ src/components/Button/ButtonVariants.js
@@ -1,13 +1,18 @@
 // src/components/ButtonVariants.js
 
-export const baseButtonClasses =
-  "inline-flex items-center justify-center rounded-full px-8 md:px-10 py-4 h6 transition-all shadow-lg cursor-pointer";
+const baseButtonClasses =
+  "inline-flex items-center justify-center hover:pulseGlow cursor-pointer";
+
+  const buttonClasses = `${baseButtonClasses} shadow-lg rounded-full px-8 md:px-10 py-4 h6`;
 
 export const ButtonVariants = {
   primary: {
-    buttonClasses: `${baseButtonClasses} bg-primary text-white hover:brightness-110`,
+    buttonClasses: `${buttonClasses} bg-primary text-white hover:brightness-110`,
   },
   secondary: {
-    buttonClasses: `${baseButtonClasses} bg-light-primary text-gray-900 hover:bg-gray-100 ring-1 ring-white/30`,
+    buttonClasses: `${buttonClasses} bg-light-primary text-gray-900 hover:bg-gray-100 ring-1 ring-white/30`,
+  },
+  link: {
+    buttonClasses: `${baseButtonClasses}`,
   },
 };
